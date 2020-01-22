f777f7050b31536a105ed3a03f379c5eb7de6337
Gerard del Castillo <gerarddelcastillo@gmail.com>
2020-01-22 23:38:06 +0100

    futile wip

--- src/components/Game/Futile/Futile.tsx
+++ src/components/Game/Futile/Futile.tsx
@@ -5,11 +5,54 @@ import React from "react"
 -ultimate
 */
 
-export default class Futile extends React.Component<{}, {}>{
+type Cell = null | "X" | "O"
+
+type Board = Cell[][]
+
+interface FutileState {
+	board: Board
+}
+
+export default class Futile extends React.Component<{}, FutileState> {
+	state = {
+		board: [
+			[null, null, null],
+			[null, null, null],
+			[null, null, null]
+		]
+	}
+
+	handleCellClick = () => {
+
+	}
+
+	renderCell(cell: Cell) {
+		return(
+			<div
+				className="Futile-component__cell"
+				onClick={this.handleCellClick}
+			>
+				{cell}
+			</div>
+		)
+	}
+
+	renderBoard() {
+		return(
+			<div className="Futile-component__board">
+			{
+				this.state.board.map(row => {
+					return row.map(cell => this.renderCell(cell))
+				})
+			}
+			</div>
+		)
+	}
+
 	render() {
 		return(
 			<div className="Futile-component">
-				Futile
+				{ this.renderBoard() }
 			</div>
 		)
 	}
